866715a1ace8e972bcbd98f76f2b190d36a032fb
sunzhaoc <45894147+sunzhaoc@users.noreply.github.com>
2019-01-29 20:06:53 +0800

    Update day02_machine_learning.py

--- Basic Machine Learning/day02_machine_learning.py
+++ Basic Machine Learning/day02_machine_learning.py
@@ -3,6 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
 
 def knn_iris():
     """
@@ -84,8 +89,76 @@ def knn_iris_gscv():
     return None
 
 
+def nb_news():
+    """
+    用朴素贝叶斯算法对新闻进行分类
+    :return:
+    """
+    # 1) 获取数据
+    news = fetch_20newsgroups(subset="all")
+    print(news)
+    # 2) 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(news.data, news.target)
+
+    # 3) 特征工程：文本特征抽取-tfidf
+    transfer = TfidfVectorizer()
+    x_train = transfer.fit(x_train)
+    x_test = transfer.transform(x_test)
+
+    # 4) 朴素贝叶斯预估器流程
+    estimator = MultinomialNB()
+    estimator.fit(x_train, y_train)
+
+    # 5）模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("直接比对真实值和预测值：\n", y_test == y_predict)
+
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    return None
+
+
+def decision_iris():
+    """
+    用决策树对鸢尾花进行分类
+    :return:
+    """
+    # 1) 获取数据集
+    iris = load_iris()
+
+    # 2) 划分数据集
+    x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=22)
+
+    # 3) 决策器预估器
+    estimator = DecisionTreeClassifier(criterion="entropy")
+    estimator.fit(x_train, y_train)
+
+    # 4) 模型评估
+    # 方法1：直接比对真实值和预测值
+    y_predict = estimator.predict(x_test)
+    print("y_predict:\n", y_predict)
+    print("直接比对真实值和预测值：\n", y_test == y_predict)
+
+    # 方法2：计算准确率
+    score = estimator.score(x_test, y_test)
+    print("准确率为：\n", score)
+
+    # 可视化决策树
+    export_graphviz(estimator, out_file="iris_tree.dot", feature_names=iris.feature_names)
+
+    return None
+
+
 if __name__ == "__main__":
     # 代码1：用KNN算法对鸢尾花进行分类
     # knn_iris()
     # 代码2： 用KNN算法对鸢尾花进行分类,添加网格搜索和交叉验证
-    knn_iris_gscv()
+    # knn_iris_gscv()
+    # 代码3：用朴素贝叶斯算法对新闻进行分类
+    nb_news()
+    # 代码4：用决策树对鸢尾花进行分类
+    # decision_iris()
